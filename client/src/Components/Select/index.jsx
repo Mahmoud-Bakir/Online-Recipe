a617c1e0c71160a9943defdbbba1a85aa9998b5d
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-08-24 23:56:22 +0300

    CLIENT <> Created Selector Component

--- client/src/Components/Select/index.jsx
+++ client/src/Components/Select/index.jsx
@@ -1,16 +1,43 @@
-import  './style.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Select=()=>{  
-    return(
+const Select = () => {
+  const [cuisines, setCuisines] = useState([]);
+
+
+  useEffect(() => {
+    const getCuisines = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const headers = {
+          Authorization: `Bearer ${token}`,
+        };
+        const response = await axios.get(
+          "http://localhost:8000/api/user/cuisines",
+          { headers }
+        );
+        console.log(response.data.data)
+        setCuisines(response.data.data)
+      } catch (error) {
+        console.error("Error fetching cuisines:", error);
+      }
+    };
+
+    getCuisines();
+  }, []);
+
+  return (
     <div className="part-selector">
-        <label htmlFor="">Cuisines</label>
-        <select className="selector" name="">
-            <option value="test">test</option>
-            <option value="test">test</option>
-            <option value="test">test</option>
-            <option value="test">test</option>
-          </select>
+      <label htmlFor="">Cuisine</label>
+      <select className="selector" name="">
+        {cuisines.map((option) => (
+          <option key={option.id} value={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
     </div>
-    );
-}
+  );
+};
+
 export default Select;
